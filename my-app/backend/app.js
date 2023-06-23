ef14dfd01f678af0ac36ec7dcf4bf955e4f092cc
leebissessar5 <leebissessar@gmail.com>
2023-06-23 07:29:03 -0400

    remove /api from endpoint urls

--- my-app/backend/app.js
+++ my-app/backend/app.js
@@ -32,13 +32,13 @@ app.use(express.json())
 app.use(middleware.tokenHandler)
 app.use(middleware.requestLogger)
 
-app.use('/api/blogs', middleware.userExtractor, blogsRouter)
-app.use('/api/users', usersRouter)
-app.use('/api/login', loginRouter)
+app.use('/blogs', middleware.userExtractor, blogsRouter)
+app.use('/users', usersRouter)
+app.use('/login', loginRouter)
 
 if (process.env.NODE_ENV === 'test') {
   const testingRouter = require('./controllers/testing')
-  app.use('/api/testing', testingRouter)
+  app.use('/testing', testingRouter)
 }
 
 app.use(middleware.unknownEndpoint)
--- my-app/frontend/src/services/users.js
+++ my-app/frontend/src/services/users.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = '/api/users'
+const baseUrl = '/users'
 
 const getAll = async () => {
   const response = await axios.get(baseUrl)
